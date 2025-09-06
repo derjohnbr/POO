6f16fad11d6115330b1a6d6b2ef25ae8eb96d2d1
DerJohnBR <derjohnbr@gmail.com>
2025-09-05 22:11:39 -0300

    Criado Algoritmo utilizando Contrutores.

---
+++ 02_Contrutores/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Contrutores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrado quadrado = new Quadrado(5);
+            quadrado.ImprimirArea();
+
+            Console.WriteLine();
+
+            Retangulo retangulo = new Retangulo(4, 6);
+            retangulo.ImprimirArea();
+
+            Console.WriteLine();
+
+            Circulo circulo = new Circulo(3);
+            circulo.ImprimirArea();
+
+            Console.WriteLine();
+
+            Triangulo triangulo = new Triangulo(4, 5);
+            triangulo.ImprimirArea();
+        }
+
+    }
+
+    public class Quadrado
+    {
+        //Atributos
+        //Modificador de acesso Private
+        //pode ser acessado apenas dentro da propria classe
+        private int Lado;
+
+        //Construtor
+        //O Construtor serve para instanciar o objeto e
+        //Inicializar os atributos
+        public Quadrado(int Lado)
+        {
+            //this: acessa o menbro da classe
+            this.Lado = Lado;
+        }
+
+        //Métodos
+        public int CalculaArea()
+        {
+            return Lado * Lado;
+        }
+
+        public void ImprimirArea()
+        {
+            Console.WriteLine($"Quadrador com lado de {Lado} possui uma área de {CalculaArea()}");
+        }
+    }
+
+    public class Retangulo
+    {
+        private int Largura, Altura;
+
+        public Retangulo(int Largura, int Altura)
+        {
+            this.Largura = Largura;
+            this.Altura = Altura;
+        }
+
+        public int CalculaArea()
+        {
+            return Largura * Altura;
+        }
+
+        public void ImprimirArea()
+        {
+            Console.WriteLine($"Retangulo com largura de {Largura} e altura de {Altura} possui uma área de {CalculaArea()}");
+        }
+    }
+
+    public class Circulo
+    {
+        private double Raio;
+        public Circulo(double Raio)
+        {
+            this.Raio = Raio;
+        }
+        public int CalculaArea()
+        {
+            return (int)(Math.PI * Raio * Raio);
+        }
+        public void ImprimirArea()
+        {
+            Console.WriteLine($"Círculo com raio de {Raio} possui uma área de {CalculaArea()}");
+        }
+    }
+
+    public class Triangulo
+    {
+        private int Base, Altura;
+        public Triangulo(int Base, int Altura)
+        {
+            this.Base = Base;
+            this.Altura = Altura;
+        }
+        public int CalculaArea()
+        {
+            return (Base * Altura) / 2;
+        }
+        public void ImprimirArea()
+        {
+            Console.WriteLine($"Triângulo com base de {Base} e altura de {Altura} possui uma área de {CalculaArea()}");
+        }
+    }
+
+}
---
+++ 02_Contrutores/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("02_Contrutores")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("02_Contrutores")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("e7412872-b97f-49ce-a039-3d238717e509")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
