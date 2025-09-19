7bf98c09f5d26f58d507188b025bf4cc68cb9ec8
DerJohnBR <derjohnbr@gmail.com>
2025-09-19 20:40:22 -0300

    Termino Herança.

--- 09_Heranca/Pessoa.cs
+++ 09_Heranca/Pessoa.cs
@@ -8,6 +8,7 @@ namespace _09_Heranca
 {
     internal abstract class Pessoa
     {
+        public int Id { get; set; }
         public string Nome;
         public string Telefone;
 
@@ -16,6 +17,26 @@ namespace _09_Heranca
         public void Imprimir()
         {
             Console.WriteLine($"LISTA DE PESSOAS\n\nNome: {this.Nome} Documento: {this.GetDocumento()} Telefone: {this.Telefone}\n");
+        }
+
+        public override string ToString()
+        {
+            return $"<ToString>: Nome: {this.Nome} Documento: {this.GetDocumento()} Telefone: {this.Telefone}\n";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null /*|| this.GetType() != obj.GetType()*/)
+                return false;
+
+            //Cast=> Conversão de Objeto
+            //((Pessoa)obj)=> Converte obj que é do tipo objeto para tipo Pessoa
+            return this.Id == ((Pessoa)obj).Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();
         }
     }
 }
