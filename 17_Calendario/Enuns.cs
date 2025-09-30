a1172bc5d85880e7e44d757f3b6cea70f02ee5e1
DerJohnBR <derjohnbr@gmail.com>
2025-09-29 22:20:07 -0300

    Alterado com Enum.

---
+++ 17_Calendario/Enuns.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_Calendario
+{
+    public enum Mes
+    {
+        Janeiro = 1,
+        Fevereiro,
+        Março,
+        Abril,
+        Maio,
+        Junho,
+        Julho,
+        Agosto,
+        Setembro,
+        Outubro,
+        Novembro,
+        Dezembro
+    }
+}
