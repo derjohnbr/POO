46aec5f9794ca844ed761a1c7b87a020ad37cab8
DerJohnBR <derjohnbr@gmail.com>
2025-10-09 19:57:38 -0300

    Criado Leitura de Arquivo com Exceção.

---
+++ 23_LeituraArquivoComExcecao/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_LeituraArquivoComExcecao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe o caminho completo do arquivo (ex.: C:\\temp\\arquivo.txt): ");
+            string caminho = Console.ReadLine();
+
+            try
+            {
+                string conteudo = File.ReadAllText(caminho);
+                Console.WriteLine("\n--- Conteúdo do arquivo ---");
+                Console.WriteLine(conteudo);
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Arquivo não encontrado! Verifique o caminho e tente novamente.");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Arquivo não encontrado! Verifique o caminho e tente novamente.");
+            }
+        }
+    }
+}
