f7b8a0ac7bcd23cf630efcb8312b7341321b3c84
DerJohnBR <derjohnbr@gmail.com>
2025-10-09 19:39:36 -0300

    Criação Conversão de Número com Exceção.

---
+++ 21_ConversaoNumeroComExcecao/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_ConversaoNumeroComExcecao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número inteiro: ");
+            try
+            { 
+                int valor = int.Parse(Console.ReadLine());
+                int dobro = valor * 2;
+                Console.WriteLine($"Dobro: {dobro}");
+            }
+                catch (FormatException)
+            {
+                Console.WriteLine("Valor inválido! Digite apenas números inteiros.");
+            }
+
+        }
+    }
+}
